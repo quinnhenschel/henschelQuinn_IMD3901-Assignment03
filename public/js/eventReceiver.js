aa53d8e374a549202321f01ecfcf8f1c2f69c36d
quinnhenschel <qhenschel@gmail.com>
2019-03-22 01:41:02 -0400

    added texture and normal map for floor and walls. added all doors and buttons to scene. finished logic for which button triggers what svg for player2 and what in a-frame for player1. added SFX. styled svg css in differnt states and locked user out in certain states. modified lighting. added fog

--- public/js/eventReceiver.js
+++ public/js/eventReceiver.js
@@ -2,46 +2,301 @@
 //default connect event
 socket.on('connect', function() {
     console.log("receiver connected!");
+    area1_available = false;
+    area2_available = false;
+    area3_available = false;
+    
 });
 
 socket.on('buttonClicked', function(clickedObj){
     clickedObj = clickedObj.clicked.clicked;
 
-    if (userID == 'player1')
+    if (clickedObj == 'startButton')
     {
-        if (clickedObj == 'startButton')
+        if (userID == 'player1')
         {
-            door = document.querySelector('#door1_1');
-            door_position = JSON.parse(JSON.stringify((door.object3D.position)));
-            door.object3D.position.set(door_position.x ,4 ,door_position.z);
+            bgSound = document.querySelector('#ambience');
+            bgSound.components['sound'].playSound();
+
+            console.log("start");
+            button = document.querySelector('#startButton');
+            button.setAttribute('material', {color: '#ff6464'});
+            button.setAttribute('material', {emissive: '#ff6464'});
+            button.setAttribute('material', {emissiveIntensity: 0.4});
         }
-        
-        if (clickedObj == 'area1StartButton')
+        if (userID == 'player2')
         {
-            door = document.querySelector('#door2_start');
-            door_position = JSON.parse(JSON.stringify((door.object3D.position)));
-            door.object3D.position.set(door_position.x , 4 ,door_position.z);
+            svgElement = document.querySelector('#areaStart');
+            svgElement.classList.remove('regular');
+            svgElement.classList.add('clickable');
+        }
+
+    }
+    
+    if (clickedObj == 'area1StartButton'){
+        if (area1_available == false){
+            area1_available = true;
+            if (userID == 'player1'){
+                button = document.querySelector('#area1StartButton');
+                button.setAttribute('material', {color: '#ff6464'});
+                button.setAttribute('material', {emissive: '#ff6464'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+            if (userID == 'player2'){
+                svgElement = document.querySelector('#area1_1');
+                svgElement.classList.remove('regular');
+                svgElement.classList.add('clickable');
+    
+                svgElement = document.querySelector('#area1_2');
+                svgElement.classList.remove('regular');
+                svgElement.classList.add('clickable');
+            }
+        }
+        else{
+            console.log("its locked rn");
         }
     }
+    
+    if (clickedObj == 'area2StartButton'){
+        if (area2_available == false){
+            area2_available = true;
+            if (userID == 'player1'){
+                button = document.querySelector('#area2StartButton');
+                button.setAttribute('material', {color: '#ff6464'});
+                button.setAttribute('material', {emissive: '#ff6464'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+            if (userID == 'player2'){
+                svgElement = document.querySelector('#area2_1');
+                svgElement.classList.remove('regular');
+                svgElement.classList.add('clickable');
+    
+                svgElement = document.querySelector('#area2_2');
+                svgElement.classList.remove('regular');
+                svgElement.classList.add('clickable');
+            }
+        }
+        else{
+            console.log("its locked rn");
+        }
+    }
+
+    if (clickedObj == 'area3StartButton'){
+        if (area3_available == false){
+            area3_available = true;
+            if (userID == 'player1'){
+                button = document.querySelector('#area3StartButton');
+                button.setAttribute('material', {color: '#ff6464'});
+                button.setAttribute('material', {emissive: '#ff6464'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+            if (userID == 'player2'){
+                svgElement = document.querySelector('#area3_1');
+                svgElement.classList.remove('regular');
+                svgElement.classList.add('clickable');
+    
+                svgElement = document.querySelector('#area3_2');
+                svgElement.classList.remove('regular');
+                svgElement.classList.add('clickable');
+            }
+        }
+        else{
+            console.log("its locked rn");
+        }
+    }
+
+
 });
 
 socket.on('svgClicked', function(id) {
     id = id.clicked.clicked;
 
-    if (id == 'area1_1')
-    {
-        //remove the door
-        if(userID == 'player1')
-        {
-            door = document.querySelector('#area_1_left');
-            door_position = JSON.parse(JSON.stringify((door.object3D.position)));
-            door.object3D.position.set(door_position.x , 4 ,door_position.z);
+    if (id == 'areaStart'){
+        if(userID == 'player1'){
+            anim = document.querySelector('#door1_openAnimation');
+            anim.emit('door1_animation_start');
+            bgSound = document.querySelector('#doorSound');
+            bgSound.components['sound'].stopSound();
+            bgSound.components['sound'].playSound();
         }
     
-        if(userID == 'player2')
-        {
+        if(userID == 'player2'){
             svgElement = document.querySelector('#' + id);
+            svgElement.classList.remove('clickable');
             svgElement.classList.add('fade');
         }
-    }   
+    }
+    
+    if (area1_available == true)
+    {
+        if (id == 'area1_1'){
+            area1_available = false;
+            if(userID == 'player1'){
+                anim = document.querySelector('#area1_door1_openAnimation');
+                anim.emit('area1_door1_animation_start');
+                bgSound = document.querySelector('#doorSound');
+                bgSound.components['sound'].stopSound();
+                bgSound.components['sound'].playSound();
+
+                button = document.querySelector('#area1StartButton');
+                button.setAttribute('material', {color: '#52C252'});
+                button.setAttribute('material', {emissive: '#52C252'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+        
+            if(userID == 'player2'){
+                svgElement = document.querySelector('#' + id);
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('fade');
+
+                svgElement = document.querySelector('#area1_2');
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('regular');
+            }
+        }
+    
+
+        if (id == 'area1_2'){
+            area1_available = false;
+            if(userID == 'player1'){
+                anim = document.querySelector('#area1_door2_openAnimation');
+                anim.emit('area1_door2_animation_start');
+                bgSound = document.querySelector('#doorSound');
+                bgSound.components['sound'].stopSound();
+                bgSound.components['sound'].playSound();
+
+                button = document.querySelector('#area1StartButton');
+                button.setAttribute('material', {color: '#52C252'});
+                button.setAttribute('material', {emissive: '#52C252'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+        
+            if(userID == 'player2'){
+                svgElement = document.querySelector('#' + id);
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('fade');
+
+                svgElement = document.querySelector('#area1_1');
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('regular');
+            }
+        }
+    }
+
+    /////////////////////////////////////////////
+
+    if(area2_available == true)
+    {
+        if (id == 'area2_1'){
+            area2_available = false;
+            if(userID == 'player1'){
+                anim = document.querySelector('#area2_door1_openAnimation');
+                anim.emit('area2_door1_animation_start');
+                bgSound = document.querySelector('#doorSound');
+                bgSound.components['sound'].stopSound();
+                bgSound.components['sound'].playSound();
+
+                button = document.querySelector('#area2StartButton');
+                button.setAttribute('material', {color: '#52C252'});
+                button.setAttribute('material', {emissive: '#52C252'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+        
+            if(userID == 'player2'){
+                svgElement = document.querySelector('#' + id);
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('fade');
+
+                svgElement = document.querySelector('#area2_2');
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('regular');
+            }
+        }
+    
+        if (id == 'area2_2'){
+            area2_available = false;
+            if(userID == 'player1'){
+                anim = document.querySelector('#area2_door2_openAnimation');
+                anim.emit('area2_door2_animation_start');
+                bgSound = document.querySelector('#doorSound');
+                bgSound.components['sound'].stopSound();
+                bgSound.components['sound'].playSound();
+
+                button = document.querySelector('#area2StartButton');
+                button.setAttribute('material', {color: '#52C252'});
+                button.setAttribute('material', {emissive: '#52C252'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+        
+            if(userID == 'player2'){
+                svgElement = document.querySelector('#' + id);
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('fade');
+
+                svgElement = document.querySelector('#area2_1');
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('regular');
+            }
+        }
+    }
+
+
+    ////////////////////////////////////////
+
+    if(area3_available == true)
+    {
+        if (id == 'area3_1'){
+            area3_available = false;
+            if(userID == 'player1'){
+                anim = document.querySelector('#area3_door1_openAnimation');
+                anim.emit('area3_door1_animation_start');
+                bgSound = document.querySelector('#doorSound');
+                bgSound.components['sound'].stopSound();
+                bgSound.components['sound'].playSound();
+
+                button = document.querySelector('#area3StartButton');
+                button.setAttribute('material', {color: '#52C252'});
+                button.setAttribute('material', {emissive: '#52C252'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+        
+            if(userID == 'player2'){
+                svgElement = document.querySelector('#' + id);
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('fade');
+
+                svgElement = document.querySelector('#area3_2');
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('regular');
+            }
+        }
+    
+        if (id == 'area3_2'){
+            area3_available = false;
+            if(userID == 'player1'){
+                anim = document.querySelector('#area3_door2_openAnimation');
+                anim.emit('area3_door2_animation_start');
+                bgSound = document.querySelector('#doorSound');
+                bgSound.components['sound'].stopSound();
+                bgSound.components['sound'].playSound();
+
+                button = document.querySelector('#area3StartButton');
+                button.setAttribute('material', {color: '#52C252'});
+                button.setAttribute('material', {emissive: '#52C252'});
+                button.setAttribute('material', {emissiveIntensity: 0.4});
+            }
+        
+            if(userID == 'player2'){
+                svgElement = document.querySelector('#' + id);
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('fade');
+
+                svgElement = document.querySelector('#area3_1');
+                svgElement.classList.remove('clickable');
+                svgElement.classList.add('regular');
+            }
+        }
+    }
+
+    
 });
--- public/js/master.js
+++ public/js/master.js
@@ -1,1 +1,6 @@
 var userID
+
+function onSceneLoad(){
+    bgSound = document.querySelector('#ambience');
+    bgSound.components['sound'].playSound();
+}
